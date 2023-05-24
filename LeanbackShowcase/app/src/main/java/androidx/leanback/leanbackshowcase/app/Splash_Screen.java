cba2852de1157659120a977cb8beb2c51d24ba03
IkramRamadhan08 <ikramramadhan0000@gmail.com>
2023-05-25 01:48:46 +0700

    Update

---
+++ LeanbackShowcase/app/src/main/java/androidx/leanback/leanbackshowcase/app/Splash_Screen.java
@@ -0,0 +1,23 @@
+package androidx.leanback.leanbackshowcase.app;
+
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.leanback.leanbackshowcase.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_Screen extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash_Screen.this,MainActivity.class);
+            Splash_Screen.this.startActivity(intent);
+            Splash_Screen.this.finish();
+        },6000);
+    }
+}
